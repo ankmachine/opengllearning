290d459dc6063cb4c6e08d7b06cdeb2f891c5985
Ankit Sinha <none>
2019-04-25 22:03:17 +0530

    made triangle !!

--- opengllearning/main.cpp
+++ opengllearning/main.cpp
@@ -28,7 +28,7 @@ const GLchar* fragmentShaderSource = "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"color = vec4(0.2f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
 
 int main(int argc, const char * argv[]) {
@@ -112,6 +112,29 @@ int main(int argc, const char * argv[]) {
     
     glDeleteShader( vertexShader );
     glDeleteShader( fragmentShader );
+    
+    // getting vertex data
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f, -0.0f,
+        0.5f, -0.5f, 0.0f, // Right
+        0.0f,  0.5f, 0.0f
+    };
+    
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray( VAO );
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // this is where i am telling opengl to how to use data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
 
     // this our game loop, we are saying here when the window is open
     while(!glfwWindowShouldClose(window))
@@ -121,9 +144,17 @@ int main(int argc, const char * argv[]) {
         
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
         glfwSwapBuffers(window);
     }
-
+    
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteBuffers( 1, &VBO );
     // we hit this part of the code only if window is closed
     glfwTerminate();
     
